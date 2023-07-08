a036b3ff6136d31b99f13f4b8235aa4dabf79d7f
Gabriel <gabrielvitor083@gmail.com>
2023-07-08 18:35:53 -0300

    update products components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./ComponentsApplication/HeaderComponents/Header";
+import Products from "./ComponentsApplication/ProductsComponents/Products";
 
 
 function App() {
   return (
     <div>
       <Header />
+      <Products />
     </div>
   );
 }
